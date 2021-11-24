06ba7638d5b3691e5fc2570ab93a96fd753386a9
jahanzebsethi <jahanzebsethi@gmail.com>
2021-11-24 20:59:11 +0500

    Server setup and logging

--- bin/www.js
+++ bin/www.js
@@ -1,11 +1,58 @@
 const http = require('http');
+
 const app = require('../server/index');
+const Logger = require('../utils/logger');
+
+const logger = new Logger();
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error) {
+	if (error.syscall !== 'listen') {
+		throw error;
+	}
+
+	const bind = typeof port === 'string'
+		? `Pipe ${port}`
+		: `Port ${port}`;
+
+	// handle specific listen errors with friendly messages
+	switch (error.code) {
+	case 'EACCES':
+		logger.log(`${bind} requires elevated privileges`);
+		process.exit(1);
+		break;
+	case 'EADDRINUSE':
+		logger.log(`${bind} is already in use`);
+		process.exit(1);
+		break;
+	default:
+		throw error;
+	}
+}
+
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening() {
+	const addr = server.address();
+	const bind = typeof addr === 'string'
+		? `pipe ${addr}`
+		: `port ${addr.port}`;
+
+	logger.log(`the server started listining on ${bind}`, 'info');
+}
 
 /**
  * Create HTTP server.
  */
- const server = http.createServer(app);
+const server = http.createServer(app);
 
- const port = process.env.PORT || 3000;
+const port = process.env.DEV_APP_PORT || 3000;
+app.set('port', port);
 
- server.listen(port);
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
